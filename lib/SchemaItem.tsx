b06fad54b6ee317fa35fac88b61059b7dd3b9f61
zt <2249269086@qq.com>
2021-02-28 23:17:28 +0800

    feat: finish 4-6

--- lib/SchemaItem.tsx
+++ lib/SchemaItem.tsx
@@ -1,22 +1,11 @@
 import { defineComponent, PropType } from 'vue'
 import NumberField from './filelds/NumberField'
-import StringField from './filelds/StringField'
-import { Schema, SchemaTypes } from './type'
+// import StringField from './filelds/StringField'
+import StringField from './filelds/StringField.vue'
+import { Schema, SchemaTypes, FiledPropsDefine } from './type'
 export default defineComponent({
   name: 'SchemaItem',
-  props: {
-    schema: {
-      type: Object as PropType<Schema>,
-      required: true,
-    },
-    value: {
-      required: true,
-    },
-    onChange: {
-      type: Function as PropType<(v: any) => void>,
-      required: true,
-    },
-  },
+  props: FiledPropsDefine,
   setup(props) {
     return () => {
       const { schema } = props
--- src/demos/demo.ts
+++ src/demos/demo.ts
@@ -4,5 +4,5 @@ export default {
     type: 'string',
   },
   uiSchema: {},
-  default: 1,
+  default: 'zt',
 }
