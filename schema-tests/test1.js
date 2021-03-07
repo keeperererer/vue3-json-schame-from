e5d93a669736d5baf4958c88ff6734d3c6d486fd
zt <2249269086@qq.com>
2021-03-07 15:00:48 +0800

    finished 7

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,15 +1,41 @@
 // or ESM/TypeScript import
 // import Ajv from 'ajv'
 // Node.js require:
-const Ajv = require('ajv').default
-const localize = require('ajv-i18n')
-
+const Ajv = require('ajv')
 const schema = {
-  type: 'string',
-  minLength: 10,
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      format: 'test',
+      // minLength: 10,
+    },
+    age: {
+      type: 'number',
+    },
+    pets: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+    },
+    isWorker: {
+      type: 'boolean',
+    },
+  },
+  required: ['name', 'age'],
 }
- 
+const data = {
+  name: 'haha',
+  age: 18,
+  // pets: ['mimi', 'mama'],
+  isWorker: true,
+}
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+ajv.addFormat('test', (data) => {
+  console.log(data, '________')
+  return data === 'haha'
+})
 const validate = ajv.compile(schema)
-const valid = validate('zt')
+const valid = validate(data)
 if (!valid) console.log(validate.errors)
